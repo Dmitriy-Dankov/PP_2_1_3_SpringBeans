d0747dc6fb6ba957e02c4a10b355bdf0e2c81618
Dmitriy Dankov <d.a.n.k.o.v@yandex.ru>
2024-12-20 12:42:54 +0300

    add Dog class, create bin Timer and link them to AnimalCage

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("CAT")
-public class Cat extends Animal {
+//@Primary
+@Qualifier("DOG")
+public class Dog extends Animal {
     @Override
     public String toString() {
-        return "Im a Cat";
+        return "Im a Dog";
     }
 }
